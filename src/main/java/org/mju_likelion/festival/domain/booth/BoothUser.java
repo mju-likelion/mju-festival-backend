fd598783fcb674ce7f7cb9be4e5335b6edb64f7e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-23 03:13:02 +0900

    feat: BoothUser Entity 작성 - #5

---
+++ src/main/java/org/mju_likelion/festival/domain/booth/BoothUser.java
@@ -0,0 +1,28 @@
+package org.mju_likelion.festival.domain.booth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mju_likelion.festival.domain.common.BaseEntity;
+import org.mju_likelion.festival.domain.user.User;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "booth_user")
+public class BoothUser extends BaseEntity {
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "booth_id", nullable = false)
+  private Booth booth;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+}
