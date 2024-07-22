9967ba3199a6bf58a999aa6193e03ce6eed53cf5
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-23 03:13:48 +0900

    feat: Announcement Entity 작성 - #5

---
+++ src/main/java/org/mju_likelion/festival/domain/announcement/Announcement.java
@@ -0,0 +1,40 @@
+package org.mju_likelion.festival.domain.announcement;
+
+import static org.mju_likelion.festival.domain.common.constant.ColumnLengths.ANNOUNCEMENT_CONTENT_LENGTH;
+import static org.mju_likelion.festival.domain.common.constant.ColumnLengths.ANNOUNCEMENT_TITLE_LENGTH;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mju_likelion.festival.domain.admin.Admin;
+import org.mju_likelion.festival.domain.common.BaseEntity;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "announcement")
+public class Announcement extends BaseEntity {
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "writer_id")
+  private Admin writer;
+
+  @Column(nullable = false, length = ANNOUNCEMENT_TITLE_LENGTH)
+  private String title;
+
+  @Column(nullable = false, length = ANNOUNCEMENT_CONTENT_LENGTH)
+  private String content;
+
+  @OneToMany(mappedBy = "announcement", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<AnnouncementImage> announcementImages;
+}
