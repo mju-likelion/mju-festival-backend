cb7c3899bcb4d112ac8c600546ca94e6b91ff276
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-23 03:03:05 +0900

    feat: BaseEntity 구현 - #5
    - id, createdAt, updatedAt 존재
    - equals, hashCode 재정의

---
+++ src/main/java/org/mju_likelion/festival/domain/common/BaseEntity.java
@@ -0,0 +1,48 @@
+package org.mju_likelion.festival.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
+  @Column(name = "id", updatable = false, unique = true, nullable = false)
+  private UUID id;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BaseEntity that = (BaseEntity) o;
+    return id.equals(that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return id.hashCode();
+  }
+}
