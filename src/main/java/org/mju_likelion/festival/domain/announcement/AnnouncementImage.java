d9f45c5bfeec7fdff4c4b0eceaea19a6bb70d74a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-23 03:14:00 +0900

    feat: AnnouncementImage Entity 작성 - #5

---
+++ src/main/java/org/mju_likelion/festival/domain/announcement/AnnouncementImage.java
@@ -0,0 +1,28 @@
+package org.mju_likelion.festival.domain.announcement;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mju_likelion.festival.domain.common.BaseEntity;
+import org.mju_likelion.festival.domain.image.Image;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "announcement_image")
+public class AnnouncementImage extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "announcement_id", nullable = false)
+  private Announcement announcement;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "image_id", nullable = false)
+  private Image image;
+}
