e6aac453344d15612879184b1c2dd6c3d9e2e898
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-01 06:27:41 +0900

    feat: equals, hashCode 메서드 제거 - #16
    - DB 저장 전까지 id가 null 이기 때문

--- src/main/java/org/mju_likelion/festival/common/domain/BaseEntity.java
+++ src/main/java/org/mju_likelion/festival/common/domain/BaseEntity.java
@@ -21,28 +21,11 @@ public abstract class BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
   @Column(name = "id", updatable = false, unique = true, nullable = false)
-  private UUID id;
+  protected UUID id;
 
   @CreatedDate
   private LocalDateTime createdAt;
 
   @LastModifiedDate
   private LocalDateTime updatedAt;
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    BaseEntity that = (BaseEntity) o;
-    return id.equals(that.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return id.hashCode();
-  }
 }
