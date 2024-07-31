333553a5033b0aa0230832e2d28d65293decd4f0
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-01 06:33:09 +0900

    feat: 학번을 통해 id 를 찾는 메서드 추가 - #16

--- src/main/java/org/mju_likelion/festival/user/domain/repository/UserJpaRepository.java
+++ src/main/java/org/mju_likelion/festival/user/domain/repository/UserJpaRepository.java
@@ -1,11 +1,16 @@
 package org.mju_likelion.festival.user.domain.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
 import java.util.UUID;
 import org.mju_likelion.festival.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, UUID> {
 
+  @Query("SELECT u.id FROM user u WHERE u.studentId = :studentId")
+  Optional<UUID> findIdByStudentId(@Param("studentId") String studentId);
 }
