2411af87549b90564f8b23da0530d1366c0dbf73
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-01 07:13:28 +0900

    feat: 관리자 로그인 DTO 클래스 추가 - #18

---
+++ src/main/java/org/mju_likelion/festival/auth/dto/request/AdminLoginRequest.java
@@ -0,0 +1,17 @@
+package org.mju_likelion.festival.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class AdminLoginRequest {
+
+  @NotBlank(message = "관리자 아이디가 누락되었습니다.")
+  private String encryptedLoginId;
+
+  @NotBlank(message = "비밀번호가 누락되었습니다.")
+  private String encryptedPassword;
+
+  @NotBlank(message = "키가 누락되었습니다.")
+  private String key;
+}
