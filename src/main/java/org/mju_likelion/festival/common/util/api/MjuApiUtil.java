5c4507c2eb2d95dfc15191d64b29e4352b3fcace
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-01 06:30:48 +0900

    feat: MSI api 호출을 위한 클래스 구현 - #16
    - RestTemplate 사용
    - 장점: 간단함, 단점: 동기 호출만 지원
    - WebClient 는 비동기 호출을 지원해 빠르다는 장점이 있지만, 해당 프로젝트에서는 동기 호출만 사용할 것임

---
+++ src/main/java/org/mju_likelion/festival/common/util/api/MjuApiUtil.java
@@ -0,0 +1,50 @@
+package org.mju_likelion.festival.common.util.api;
+
+import static org.mju_likelion.festival.common.exception.type.ErrorType.API_ERROR;
+
+import lombok.RequiredArgsConstructor;
+import org.mju_likelion.festival.common.exception.InternalServerException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class MjuApiUtil {
+
+  private final RestTemplate restTemplate;
+
+  @Value("${mju-user-check-api-uri}")
+  private String mjuUserCheckApiUri;
+
+  public boolean doUserCheck(String studentId, String password) {
+    try {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+      MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+      body.add("id", studentId);
+      body.add("passwrd", password);
+
+      HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+      ResponseEntity<ApiResponse> responseEntity = restTemplate.postForEntity(mjuUserCheckApiUri,
+          request, ApiResponse.class);
+
+      ApiResponse response = responseEntity.getBody();
+      if (response != null) {
+        return "0000".equals(response.getError());
+      }
+
+      throw new InternalServerException(API_ERROR, "Response body is null");
+    } catch (Exception e) {
+      throw new InternalServerException(API_ERROR, e.getMessage());
+    }
+  }
+}
