d6c03790909ff354f865f37013be33d6311140cc
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-02 20:39:09 +0900

    feat: Order 를 기준으로 정렬된 Term 반환 함수 추가 - #29

--- src/main/java/org/mju_likelion/festival/term/domain/repository/TermJpaRepository.java
+++ src/main/java/org/mju_likelion/festival/term/domain/repository/TermJpaRepository.java
@@ -1,5 +1,6 @@
 package org.mju_likelion.festival.term.domain.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.mju_likelion.festival.term.domain.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TermJpaRepository extends JpaRepository<Term, UUID> {
 
+  /**
+   * Term 의 Order 를 기준으로 오름차순으로 정렬하여 모든 Term 조회.
+   *
+   * @return Term 의 Order 를 기준으로 오름차순으로 정렬된 모든 Term
+   */
+  List<Term> findTermsByOrderByOrderAsc();
 }
