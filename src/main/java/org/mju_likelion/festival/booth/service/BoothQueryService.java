711f630a4b837c2ef98e9f9545c08d74103069e3
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-03 03:07:16 +0900

    feat: 부스 전체 조회, 단건 조회 기능 구현 - #31

---
+++ src/main/java/org/mju_likelion/festival/booth/service/BoothQueryService.java
@@ -0,0 +1,35 @@
+package org.mju_likelion.festival.booth.service;
+
+import static org.mju_likelion.festival.common.exception.type.ErrorType.BOOTH_NOT_FOUND;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.booth.domain.repository.BoothQueryRepository;
+import org.mju_likelion.festival.booth.dto.response.BoothDetailResponse;
+import org.mju_likelion.festival.booth.dto.response.SimpleBoothResponse;
+import org.mju_likelion.festival.common.exception.NotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Transactional(readOnly = true)
+public class BoothQueryService {
+
+  private final BoothQueryRepository boothQueryRepository;
+
+  public List<SimpleBoothResponse> getBooths(final int page, final int size) {
+    return boothQueryRepository.findOrderedSimpleBoothsWithPagination(page, size)
+        .stream().map(SimpleBoothResponse::from).toList();
+  }
+
+  public BoothDetailResponse getBooth(final UUID id) {
+    return BoothDetailResponse.from(
+        boothQueryRepository.findBoothById(id).orElseThrow(
+            () -> new NotFoundException(BOOTH_NOT_FOUND)
+        )
+    );
+  }
+}
+
