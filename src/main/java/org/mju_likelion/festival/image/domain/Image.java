33bc5eb9fce6b04f70690553f2a702dc3c3a77fe
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-03 07:03:35 +0900

    feat: url 생성자 추가 - #35

--- src/main/java/org/mju_likelion/festival/image/domain/Image.java
+++ src/main/java/org/mju_likelion/festival/image/domain/Image.java
@@ -7,9 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.mju_likelion.festival.announcement.domain.AnnouncementImage;
@@ -19,24 +19,22 @@ import org.mju_likelion.festival.common.domain.BaseEntity;
 import org.mju_likelion.festival.lost_item.domain.LostItemImage;
 
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "image")
 public class Image extends BaseEntity {
 
+  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
+  private final List<LostItemImage> lostItemImages = new ArrayList<>();
+  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
+  private final List<BoothImage> boothImages = new ArrayList<>();
+  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
+  private final List<AnnouncementImage> announcementImages = new ArrayList<>();
   @Column(nullable = false, length = IMAGE_URL_LENGTH, unique = true)
   private String url;
-
   @OneToOne(mappedBy = "thumbnail", fetch = FetchType.LAZY)
   private Booth booth;
 
-  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
-  private List<LostItemImage> lostItemImages;
-
-  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
-  private List<BoothImage> boothImages;
-
-  @OneToMany(mappedBy = "image", fetch = FetchType.LAZY)
-  private List<AnnouncementImage> announcementImages;
+  public Image(String url) {
+    this.url = url;
+  }
 }
