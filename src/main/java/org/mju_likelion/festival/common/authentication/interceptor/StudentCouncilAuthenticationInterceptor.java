b894013af3c8a03abb5cdaa0ae55e5284fcbad32
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-03 06:37:23 +0900

    feat: 학생회만 접근 가능한 인터셉터 구현 - #36

---
+++ src/main/java/org/mju_likelion/festival/common/authentication/interceptor/StudentCouncilAuthenticationInterceptor.java
@@ -0,0 +1,30 @@
+package org.mju_likelion.festival.common.authentication.interceptor;
+
+import static org.mju_likelion.festival.common.exception.type.ErrorType.STUDENT_COUNCIL_ONLY_ERROR;
+
+import java.util.Objects;
+import org.mju_likelion.festival.auth.util.jwt.AuthenticationRole;
+import org.mju_likelion.festival.auth.util.jwt.JwtUtil;
+import org.mju_likelion.festival.auth.util.jwt.Payload;
+import org.mju_likelion.festival.common.authentication.AuthenticationContext;
+import org.mju_likelion.festival.common.exception.type.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentCouncilAuthenticationInterceptor extends AbstractAuthenticationInterceptor {
+
+  public StudentCouncilAuthenticationInterceptor(AuthenticationContext authenticationContext,
+      JwtUtil userJwtUtil) {
+    super(authenticationContext, userJwtUtil);
+  }
+
+  @Override
+  protected boolean isAuthorized(Payload payload) {
+    return Objects.equals(payload.getRole(), AuthenticationRole.STUDENT_COUNCIL);
+  }
+
+  @Override
+  protected ErrorType getErrorType() {
+    return STUDENT_COUNCIL_ONLY_ERROR;
+  }
+}
