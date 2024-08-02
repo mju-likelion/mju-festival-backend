627a0fb16ff79b5c51c56874f7acc14e29b601a3
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-03 03:07:34 +0900

    feat: 부스 전체 조회, 단건 조회 API 추가 - #31

---
+++ src/main/java/org/mju_likelion/festival/booth/controller/BoothController.java
@@ -0,0 +1,34 @@
+package org.mju_likelion.festival.booth.controller;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.booth.dto.response.BoothDetailResponse;
+import org.mju_likelion.festival.booth.dto.response.SimpleBoothResponse;
+import org.mju_likelion.festival.booth.service.BoothQueryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/booths")
+public class BoothController {
+
+  private final BoothQueryService boothQueryService;
+
+  @GetMapping
+  public ResponseEntity<List<SimpleBoothResponse>> getBooths(
+      @RequestParam final int page,
+      @RequestParam final int size) {
+    return ResponseEntity.ok(boothQueryService.getBooths(page, size));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<BoothDetailResponse> getBooth(@PathVariable final UUID id) {
+    return ResponseEntity.ok(boothQueryService.getBooth(id));
+  }
+}
