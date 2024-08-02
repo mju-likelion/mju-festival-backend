4f9ecff86ff8aae87b31174b6b11b3582ea7e573
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-03 07:07:32 +0900

    feat: 이미지 업로드 기능 구현 - #35

---
+++ src/main/java/org/mju_likelion/festival/image/service/ImageService.java
@@ -0,0 +1,24 @@
+package org.mju_likelion.festival.image.service;
+
+import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.image.domain.Image;
+import org.mju_likelion.festival.image.domain.repository.ImageJpaRepository;
+import org.mju_likelion.festival.image.util.s3.ImageType;
+import org.mju_likelion.festival.image.util.s3.S3ImageService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@AllArgsConstructor
+public class ImageService {
+
+  private final S3ImageService s3ImageService;
+  private final ImageJpaRepository imageJpaRepository;
+
+  public void saveImage(MultipartFile image, ImageType type) {
+    String url = s3ImageService.saveImage(image, type);
+
+    Image newImage = new Image(url);
+    imageJpaRepository.save(newImage);
+  }
+}
