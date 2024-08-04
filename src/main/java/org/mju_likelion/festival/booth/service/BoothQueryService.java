1282551f11f6f1c803e2b8b7a5bfa5cc775e2b3f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-05 03:46:49 +0900

    feat: 부스 QR 발급 기능 구현 - #50

--- src/main/java/org/mju_likelion/festival/booth/service/BoothQueryService.java
+++ src/main/java/org/mju_likelion/festival/booth/service/BoothQueryService.java
@@ -7,7 +7,10 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.mju_likelion.festival.booth.domain.repository.BoothQueryRepository;
 import org.mju_likelion.festival.booth.dto.response.BoothDetailResponse;
+import org.mju_likelion.festival.booth.dto.response.BoothQrResponse;
 import org.mju_likelion.festival.booth.dto.response.SimpleBoothResponse;
+import org.mju_likelion.festival.booth.util.qr.BoothQrManager;
+import org.mju_likelion.festival.booth.util.qr.BoothQrManagerContext;
 import org.mju_likelion.festival.common.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BoothQueryService {
 
   private final BoothQueryRepository boothQueryRepository;
+  private final BoothQrManagerContext boothQrManagerContext;
 
   public List<SimpleBoothResponse> getBooths(final int page, final int size) {
     return boothQueryRepository.findOrderedSimpleBoothsWithPagination(page, size)
@@ -30,6 +34,19 @@ public class BoothQueryService {
             () -> new NotFoundException(BOOTH_NOT_FOUND_ERROR)
         )
     );
+  }
+
+  public BoothQrResponse getBoothQr(final UUID id) {
+    BoothQrManager boothQrManager = boothQrManagerContext.boothQrManager();
+
+    validateBooth(id);
+    return new BoothQrResponse(boothQrManager.generateBoothQr(id));
+  }
+
+  private void validateBooth(final UUID id) {
+    if (!boothQueryRepository.existsById(id)) {
+      throw new NotFoundException(BOOTH_NOT_FOUND_ERROR);
+    }
   }
 }
 
