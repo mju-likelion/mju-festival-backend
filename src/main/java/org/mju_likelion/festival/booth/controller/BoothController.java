7bfbb51c1d9c5058e45fb53f8937c57bb61ddb98
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-05 05:44:03 +0900

    feat: 부스 QR 방문 처리 API 사용자만 가능하도록 적용 - #52

--- src/main/java/org/mju_likelion/festival/booth/controller/BoothController.java
+++ src/main/java/org/mju_likelion/festival/booth/controller/BoothController.java
@@ -3,14 +3,17 @@ package org.mju_likelion.festival.booth.controller;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.booth.domain.BoothQrStrategy;
 import org.mju_likelion.festival.booth.dto.response.BoothDetailResponse;
 import org.mju_likelion.festival.booth.dto.response.BoothQrResponse;
 import org.mju_likelion.festival.booth.dto.response.SimpleBoothResponse;
 import org.mju_likelion.festival.booth.service.BoothQueryService;
+import org.mju_likelion.festival.booth.service.BoothService;
 import org.mju_likelion.festival.common.authentication.AuthenticationPrincipal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoothController {
 
   private final BoothQueryService boothQueryService;
+  private final BoothService boothService;
 
   @GetMapping
   public ResponseEntity<List<SimpleBoothResponse>> getBooths(
@@ -38,5 +42,13 @@ public class BoothController {
   public ResponseEntity<BoothQrResponse> getBoothQr(@PathVariable final UUID id,
       @AuthenticationPrincipal final UUID boothAdminId) {
     return ResponseEntity.ok(boothQueryService.getBoothQr(id, boothAdminId));
+  }
+
+  @PostMapping("/{qrId}/visit")
+  public ResponseEntity<Void> visitBooth(@PathVariable final String qrId,
+      @RequestParam final BoothQrStrategy strategy,
+      @AuthenticationPrincipal final UUID userId) {
+    boothService.visitBooth(qrId, strategy, userId);
+    return ResponseEntity.ok().build();
   }
 }
