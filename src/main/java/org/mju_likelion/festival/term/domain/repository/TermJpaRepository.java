fce0f39f175898160a815bcaf79016f39fa8b25a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-05 00:25:53 +0900

    refactor: Query Repository 구현에 따른 메서드 제거 - #48

--- src/main/java/org/mju_likelion/festival/term/domain/repository/TermJpaRepository.java
+++ src/main/java/org/mju_likelion/festival/term/domain/repository/TermJpaRepository.java
@@ -1,6 +1,5 @@
 package org.mju_likelion.festival.term.domain.repository;
 
-import java.util.List;
 import java.util.UUID;
 import org.mju_likelion.festival.term.domain.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,10 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TermJpaRepository extends JpaRepository<Term, UUID> {
 
-  /**
-   * Term 의 Order 를 기준으로 오름차순으로 정렬하여 모든 Term 조회.
-   *
-   * @return Term 의 Order 를 기준으로 오름차순으로 정렬된 모든 Term
-   */
-  List<Term> findTermsByOrderBySequenceAsc();
 }
