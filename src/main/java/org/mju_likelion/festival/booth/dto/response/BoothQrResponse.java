c140964a85c444ee353ca680f048243aa5b18f2d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-05 03:46:09 +0900

    feat: 부스 QR 생성 요청 응답 DTO 추가 - #50

---
+++ src/main/java/org/mju_likelion/festival/booth/dto/response/BoothQrResponse.java
@@ -0,0 +1,14 @@
+package org.mju_likelion.festival.booth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 부스 QR 생성 응답 DTO.
+ */
+@Getter
+@AllArgsConstructor
+public class BoothQrResponse {
+
+  private final String qrCode;
+}
