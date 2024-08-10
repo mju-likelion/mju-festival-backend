831309b70ba5f820181d18949a04b1180b939347
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-11 00:43:19 +0900

    refactor: 총 페이지 수 반환 추가함에 따른 클래스 교체 - #64

---
+++ src/main/java/org/mju_likelion/festival/announcement/dto/response/SimpleAnnouncementsResponse.java
@@ -0,0 +1,22 @@
+package org.mju_likelion.festival.announcement.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.mju_likelion.festival.announcement.domain.SimpleAnnouncement;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SimpleAnnouncementsResponse {
+
+  private final List<SimpleAnnouncement> simpleAnnouncements;
+  private final int totalPage;
+
+  public static SimpleAnnouncementsResponse from(
+      List<SimpleAnnouncement> simpleAnnouncements,
+      int totalPage) {
+
+    return new SimpleAnnouncementsResponse(simpleAnnouncements, totalPage);
+  }
+}
