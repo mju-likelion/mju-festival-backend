8b348c7640920bc7ed176e080ae8e5192a2565ae
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-11 00:46:49 +0900

    refactor: 공지사항 전체 조회 시 총 페이지 수도 반환하도록 리팩터링 - #64
    - SimpleAnnouncementsResponse 를 반환하도록 변경

--- src/main/java/org/mju_likelion/festival/announcement/service/AnnouncementQueryService.java
+++ src/main/java/org/mju_likelion/festival/announcement/service/AnnouncementQueryService.java
@@ -1,9 +1,13 @@
 package org.mju_likelion.festival.announcement.service;
 
+import static org.mju_likelion.festival.common.exception.type.ErrorType.PAGE_OUT_OF_BOUND_ERROR;
+
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.announcement.domain.SimpleAnnouncement;
 import org.mju_likelion.festival.announcement.domain.repository.AnnouncementQueryRepository;
-import org.mju_likelion.festival.announcement.dto.response.SimpleAnnouncementResponse;
+import org.mju_likelion.festival.announcement.dto.response.SimpleAnnouncementsResponse;
+import org.mju_likelion.festival.common.exception.NotFoundException;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +19,21 @@ public class AnnouncementQueryService {
 
   private final AnnouncementQueryRepository announcementQueryRepository;
 
-  public List<SimpleAnnouncementResponse> getAnnouncements(Direction sort, int page, int size) {
-    return announcementQueryRepository.findSimpleAnnouncements(sort, page, size).stream()
-        .map(SimpleAnnouncementResponse::from)
-        .toList();
+  public SimpleAnnouncementsResponse getAnnouncements(Direction sort, int page, int size) {
+
+    int totalPage = announcementQueryRepository.getTotalPage(size);
+
+    validatePage(page, totalPage);
+
+    List<SimpleAnnouncement> simpleAnnouncements = announcementQueryRepository
+        .findSimpleAnnouncements(sort, page, size);
+
+    return SimpleAnnouncementsResponse.from(simpleAnnouncements, totalPage);
+  }
+
+  private void validatePage(int page, int totalPage) {
+    if (page >= totalPage) {
+      throw new NotFoundException(PAGE_OUT_OF_BOUND_ERROR);
+    }
   }
 }
