bbc33583cc3ddc87d2d0703c4c3415cd77947b85
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 05:18:54 +0900

    feat: 공지사항 생성 요청 DTO 작성 - #72
    - domain 인 Announcement 의 속성 길이를 DTO 에서 검증하는 것은 옳지 않다고 판단해 Null 여부만 검증
    - 이미지는 없을 수 있으므로 검사하지 않음

---
+++ src/main/java/org/mju_likelion/festival/announcement/dto/request/CreateAnnouncementRequest.java
@@ -0,0 +1,19 @@
+package org.mju_likelion.festival.announcement.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+/**
+ * 공지사항 생성 요청 DTO
+ */
+@Getter
+public class CreateAnnouncementRequest {
+
+  @NotNull(message = "제목은 필수 입력값입니다.")
+  private String title;
+
+  @NotNull(message = "내용은 필수 입력값입니다.")
+  private String content;
+
+  private String imageUrl;
+}
