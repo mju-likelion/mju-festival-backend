7854f6559bd6ae5383090eff9fd7659ccf11370f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 05:15:50 +0900

    feat: getAllowedRequestMatchers 오버라이딩 구현 - #72
    - GET /announcements 에서 통과
    - GET /announcements/{id} 에서 통과
    - URI 중복으로 인한 작업

--- src/main/java/org/mju_likelion/festival/common/authentication/interceptor/StudentCouncilAuthenticationInterceptor.java
+++ src/main/java/org/mju_likelion/festival/common/authentication/interceptor/StudentCouncilAuthenticationInterceptor.java
@@ -2,12 +2,16 @@ package org.mju_likelion.festival.common.authentication.interceptor;
 
 import static org.mju_likelion.festival.common.exception.type.ErrorType.STUDENT_COUNCIL_ONLY_ERROR;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 import org.mju_likelion.festival.auth.util.jwt.AuthenticationRole;
 import org.mju_likelion.festival.auth.util.jwt.JwtUtil;
 import org.mju_likelion.festival.auth.util.jwt.Payload;
 import org.mju_likelion.festival.common.authentication.AuthenticationContext;
+import org.mju_likelion.festival.common.config.RequestMatcher;
 import org.mju_likelion.festival.common.exception.type.ErrorType;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +20,15 @@ public class StudentCouncilAuthenticationInterceptor extends AbstractAuthenticat
   public StudentCouncilAuthenticationInterceptor(AuthenticationContext authenticationContext,
       JwtUtil userJwtUtil) {
     super(authenticationContext, userJwtUtil);
+  }
+
+  @Override
+  protected List<RequestMatcher> getAllowedRequestMatchers() {
+    List<RequestMatcher> allowedRequestMatchers = new LinkedList<>();
+    allowedRequestMatchers.add(new RequestMatcher(HttpMethod.GET, "/announcements"));
+    allowedRequestMatchers.add(new RequestMatcher(HttpMethod.GET, "/announcements/{id}"));
+
+    return allowedRequestMatchers;
   }
 
   @Override
