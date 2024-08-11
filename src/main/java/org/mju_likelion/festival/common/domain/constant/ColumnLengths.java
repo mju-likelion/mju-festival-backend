76d424b201eba06730b8d69e40ff23dea68467d9
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 05:07:58 +0900

    refactor: 공지사항 컬럼 길이 변경 - #72

--- src/main/java/org/mju_likelion/festival/common/domain/constant/ColumnLengths.java
+++ src/main/java/org/mju_likelion/festival/common/domain/constant/ColumnLengths.java
@@ -20,8 +20,8 @@ public abstract class ColumnLengths {
   public static final int BOOTH_DESCRIPTION_LENGTH = 4000;
   public static final int BOOTH_LOCATION_LENGTH = 100;
 
-  public static final int ANNOUNCEMENT_TITLE_LENGTH = 100;
-  public static final int ANNOUNCEMENT_CONTENT_LENGTH = 4000;
+  public static final int ANNOUNCEMENT_TITLE_LENGTH = 50;
+  public static final int ANNOUNCEMENT_CONTENT_LENGTH = 100;
 
   public static final int USER_STUDENT_ID_LENGTH = 8;
 }
