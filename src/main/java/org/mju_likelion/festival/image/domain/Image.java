31e1fd2eb941ac35cba88bace68c431f448300d7
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 05:09:04 +0900

    refactor: 양방향 매핑 해제 및 validate 메서드 추가 - #72
    - 양방향 매핑을 할 필요성이 없다고 판단

--- src/main/java/org/mju_likelion/festival/image/domain/Image.java
+++ src/main/java/org/mju_likelion/festival/image/domain/Image.java
@@ -1,40 +1,33 @@
 package org.mju_likelion.festival.image.domain;
 
 import static org.mju_likelion.festival.common.domain.constant.ColumnLengths.IMAGE_URL_LENGTH;
+import static org.mju_likelion.festival.common.exception.type.ErrorType.INVALID_IMAGE_URL_LENGTH_ERROR;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.mju_likelion.festival.announcement.domain.Announcement;
-import org.mju_likelion.festival.booth.domain.Booth;
 import org.mju_likelion.festival.common.domain.BaseEntity;
-import org.mju_likelion.festival.lost_item.domain.LostItem;
+import org.mju_likelion.festival.common.exception.BadRequestException;
+import org.mju_likelion.festival.common.util.string.StringUtil;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "image")
 public class Image extends BaseEntity {
 
-  @OneToOne(mappedBy = "image", fetch = FetchType.LAZY)
-  private Booth boothThumbnail;
-
-  @OneToOne(mappedBy = "image", fetch = FetchType.LAZY)
-  private Booth boothImage;
-
-  @OneToOne(mappedBy = "image", fetch = FetchType.LAZY)
-  private LostItem lostItem;
-
-  @OneToOne(mappedBy = "image", fetch = FetchType.LAZY)
-  private Announcement announcement;
-
-  @Column(nullable = false, length = IMAGE_URL_LENGTH, unique = true)
+  @Column(nullable = false, length = IMAGE_URL_LENGTH)
   private String url;
 
   public Image(String url) {
+    validateUrl(url);
     this.url = url;
+  }
+
+  public void validateUrl(String url) {
+    if (StringUtil.isEmptyOrLargerThan(url, IMAGE_URL_LENGTH)) {
+      throw new BadRequestException(INVALID_IMAGE_URL_LENGTH_ERROR);
+    }
   }
 }
