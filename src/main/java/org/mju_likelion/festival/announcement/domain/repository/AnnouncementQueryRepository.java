54b19179ce79281990f7ab0e0284e3ff9892c595
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 01:36:17 +0900

    feat: 공지사항 상세 정보 조회 함수 추가 - #68

--- src/main/java/org/mju_likelion/festival/announcement/domain/repository/AnnouncementQueryRepository.java
+++ src/main/java/org/mju_likelion/festival/announcement/domain/repository/AnnouncementQueryRepository.java
@@ -1,11 +1,15 @@
 package org.mju_likelion.festival.announcement.domain.repository;
 
 import static org.mju_likelion.festival.common.util.uuid.UUIDUtil.hexToUUID;
+import static org.mju_likelion.festival.common.util.uuid.UUIDUtil.uuidToHex;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.mju_likelion.festival.announcement.domain.AnnouncementDetail;
 import org.mju_likelion.festival.announcement.domain.SimpleAnnouncement;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -17,6 +21,7 @@ import org.springframework.stereotype.Repository;
 public class AnnouncementQueryRepository {
 
   private final RowMapper<SimpleAnnouncement> simpleAnnouncementRowMapper = simpleAnnouncementRowMapper();
+  private final RowMapper<AnnouncementDetail> announcementDetailRowMapper = announcementDetailRowMapper();
   private final NamedParameterJdbcTemplate jdbcTemplate;
 
   private RowMapper<SimpleAnnouncement> simpleAnnouncementRowMapper() {
@@ -31,6 +36,19 @@ public class AnnouncementQueryRepository {
     };
   }
 
+  private RowMapper<AnnouncementDetail> announcementDetailRowMapper() {
+    return (rs, rowNum) -> {
+      String hexId = rs.getString("announcementId");
+      UUID uuid = hexToUUID(hexId);
+      return new AnnouncementDetail(
+          uuid,
+          rs.getString("title"),
+          rs.getString("content"),
+          rs.getString("imageUrl")
+      );
+    };
+  }
+
   /**
    * 페이지네이션을 적용하여 공지사항 간단 정보 List 조회.
    *
@@ -39,7 +57,8 @@ public class AnnouncementQueryRepository {
    * @param size      크기
    * @return 공지사항 간단 정보 List
    */
-  public List<SimpleAnnouncement> findSimpleAnnouncements(Direction direction, final int page,
+  public List<SimpleAnnouncement> findOrderedSimpleAnnouncementsWithPagenation(Direction direction,
+      final int page,
       final int size) {
     String sql = "SELECT HEX(a.id) AS announcementId, a.title AS title, a.content AS content "
         + "FROM announcement a "
@@ -51,6 +70,27 @@ public class AnnouncementQueryRepository {
         .addValue("offset", page * size);
 
     return jdbcTemplate.query(sql, params, simpleAnnouncementRowMapper);
+  }
+
+  /**
+   * 공지사항 상세 정보 조회.
+   *
+   * @param id 공지사항 ID
+   */
+  public Optional<AnnouncementDetail> findAnnouncementById(final UUID id) {
+    String sql =
+        "SELECT HEX(a.id) AS announcementId, a.title AS title, a.content AS content,  i.url AS imageUrl "
+            + "FROM announcement a "
+            + "LEFT JOIN image i ON a.image_id = i.id "
+            + "WHERE a.id = UNHEX(:id)";
+
+    MapSqlParameterSource params = new MapSqlParameterSource()
+        .addValue("id", uuidToHex(id));
+
+    return Optional
+        .ofNullable(
+            DataAccessUtils.singleResult(
+                jdbcTemplate.query(sql, params, announcementDetailRowMapper)));
   }
 
   /**
