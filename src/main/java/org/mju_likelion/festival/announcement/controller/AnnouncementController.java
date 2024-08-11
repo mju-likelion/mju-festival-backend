d70892c745cbdf63cf351546f0f643f6d0056db6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 01:36:49 +0900

    feat: 공지사항 상세 조회 API 추가 - #68

--- src/main/java/org/mju_likelion/festival/announcement/controller/AnnouncementController.java
+++ src/main/java/org/mju_likelion/festival/announcement/controller/AnnouncementController.java
@@ -1,11 +1,14 @@
 package org.mju_likelion.festival.announcement.controller;
 
+import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.announcement.dto.response.AnnouncementDetailResponse;
 import org.mju_likelion.festival.announcement.dto.response.SimpleAnnouncementsResponse;
 import org.mju_likelion.festival.announcement.service.AnnouncementQueryService;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,11 @@ public class AnnouncementController {
 
     return ResponseEntity.ok(
         announcementQueryService.getAnnouncements(Direction.fromString(sort), page, size));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<AnnouncementDetailResponse> getAnnouncement(@PathVariable UUID id) {
+
+    return ResponseEntity.ok(announcementQueryService.getAnnouncement(id));
   }
 }
