be77a78253c7e043fd1f0451523b9baff8cd2091
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-12 09:52:39 +0900

    feat: 공지사항 수정 기능 추가 - #74

--- src/main/java/org/mju_likelion/festival/announcement/service/AnnouncementService.java
+++ src/main/java/org/mju_likelion/festival/announcement/service/AnnouncementService.java
@@ -1,6 +1,7 @@
 package org.mju_likelion.festival.announcement.service;
 
 import static org.mju_likelion.festival.common.exception.type.ErrorType.ADMIN_NOT_FOUND_ERROR;
+import static org.mju_likelion.festival.common.exception.type.ErrorType.ANNOUNCEMENT_NOT_FOUND_ERROR;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,7 +11,7 @@ import org.mju_likelion.festival.admin.domain.repository.AdminJpaRepository;
 import org.mju_likelion.festival.announcement.domain.Announcement;
 import org.mju_likelion.festival.announcement.domain.repository.AnnouncementJpaRepository;
 import org.mju_likelion.festival.announcement.dto.request.CreateAnnouncementRequest;
-import org.mju_likelion.festival.booth.domain.repository.BoothJpaRepository;
+import org.mju_likelion.festival.announcement.dto.request.UpdateAnnouncementRequest;
 import org.mju_likelion.festival.common.exception.NotFoundException;
 import org.mju_likelion.festival.image.domain.Image;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,6 @@ public class AnnouncementService {
 
   private final AnnouncementJpaRepository announcementJpaRepository;
   private final AdminJpaRepository adminJpaRepository;
-  private final BoothJpaRepository boothJpaRepository;
 
   public void createAnnouncement(CreateAnnouncementRequest createAnnouncementRequest,
       UUID adminId) {
@@ -29,21 +29,51 @@ public class AnnouncementService {
     Admin admin = getExistingAdmin(adminId);
 
     Image image = Optional.ofNullable(createAnnouncementRequest.getImageUrl())
-        .map(Image::new)
-        .orElse(null);
+        .map(Image::new).orElse(null);
 
-    Announcement announcement = new Announcement(
-        createAnnouncementRequest.getTitle(),
-        createAnnouncementRequest.getContent(),
-        image,
-        admin
-    );
+    Announcement announcement = new Announcement(createAnnouncementRequest.getTitle(),
+        createAnnouncementRequest.getContent(), image, admin);
 
     announcementJpaRepository.save(announcement);
   }
 
-  public Admin getExistingAdmin(UUID adminId) {
+  public void updateAnnouncement(UUID announcementId,
+      UpdateAnnouncementRequest updateAnnouncementRequest, UUID adminId) {
+
+    Announcement announcement = getExistingAnnouncement(announcementId);
+
+    validateAdminExistence(adminId);
+
+    // title 은 null 을 허용하지 않는다.
+    updateAnnouncementRequest.getTitle()
+        .doIfPresentAndNotNull(announcement::updateTitle);
+
+    // content 는 null 을 허용하지 않는다.
+    updateAnnouncementRequest.getContent()
+        .doIfPresentAndNotNull(announcement::updateContent);
+
+    // imageUrl 은 null 을 허용한다.
+    updateAnnouncementRequest.getImageUrl().doIfPresent(imageUrl -> {
+      Image image = Optional.ofNullable(imageUrl).map(Image::new).orElse(null);
+      announcement.updateImage(image);
+    });
+
+    announcementJpaRepository.save(announcement);
+  }
+
+  private void validateAdminExistence(UUID adminId) {
+    if (!adminJpaRepository.existsById(adminId)) {
+      throw new NotFoundException(ADMIN_NOT_FOUND_ERROR);
+    }
+  }
+
+  private Admin getExistingAdmin(UUID adminId) {
     return adminJpaRepository.findById(adminId)
         .orElseThrow(() -> new NotFoundException(ADMIN_NOT_FOUND_ERROR));
+  }
+
+  private Announcement getExistingAnnouncement(UUID announcementId) {
+    return announcementJpaRepository.findById(announcementId)
+        .orElseThrow(() -> new NotFoundException(ANNOUNCEMENT_NOT_FOUND_ERROR));
   }
 }
