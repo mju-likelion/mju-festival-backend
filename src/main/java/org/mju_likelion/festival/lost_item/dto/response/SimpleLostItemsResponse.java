31684d657aa937503e806399b26fa4ebfa822fe6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 14:22:44 +0900

    feat: 분실물 간단 정보 응답 DTO 추가 - #88

---
+++ src/main/java/org/mju_likelion/festival/lost_item/dto/response/SimpleLostItemsResponse.java
@@ -0,0 +1,25 @@
+package org.mju_likelion.festival.lost_item.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.mju_likelion.festival.lost_item.domain.SimpleLostItem;
+
+/**
+ * 분실물 간단 정보 응답 DTO.
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SimpleLostItemsResponse {
+
+  private final List<SimpleLostItem> simpleLostItems;
+  private final int totalPage;
+
+  public static SimpleLostItemsResponse of(
+      final List<SimpleLostItem> simpleLostItem,
+      final int totalPage) {
+
+    return new SimpleLostItemsResponse(simpleLostItem, totalPage);
+  }
+}
