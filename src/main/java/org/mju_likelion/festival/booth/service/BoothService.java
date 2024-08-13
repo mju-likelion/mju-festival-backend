8af9f56022ba60860863643b370ebf7ce34e31d3
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 11:50:41 +0900

    feat: 부스 정보 수정 기능 구현 - #84

--- src/main/java/org/mju_likelion/festival/booth/service/BoothService.java
+++ src/main/java/org/mju_likelion/festival/booth/service/BoothService.java
@@ -1,16 +1,17 @@
 package org.mju_likelion.festival.booth.service;
 
-import static org.mju_likelion.festival.common.exception.type.ErrorType.BOOTH_NOT_FOUND_ERROR;
-import static org.mju_likelion.festival.common.exception.type.ErrorType.USER_NOT_FOUND_ERROR;
+import static org.mju_likelion.festival.common.util.null_handler.NullHandler.doIfNotNull;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.admin.domain.Admin;
 import org.mju_likelion.festival.booth.domain.Booth;
 import org.mju_likelion.festival.booth.domain.BoothQrStrategy;
 import org.mju_likelion.festival.booth.domain.repository.BoothJpaRepository;
+import org.mju_likelion.festival.booth.dto.request.UpdateBoothRequest;
 import org.mju_likelion.festival.booth.util.qr.BoothQrManager;
 import org.mju_likelion.festival.booth.util.qr.BoothQrManagerContext;
-import org.mju_likelion.festival.common.exception.NotFoundException;
+import org.mju_likelion.festival.image.domain.Image;
 import org.mju_likelion.festival.user.domain.User;
 import org.mju_likelion.festival.user.domain.repository.UserJpaRepository;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ public class BoothService {
   private final BoothQrManagerContext boothQrManagerContext;
   private final BoothJpaRepository boothJpaRepository;
   private final UserJpaRepository userJpaRepository;
+  private final BoothServiceUtils boothServiceUtils;
 
   public void visitBooth(final String qrId, final BoothQrStrategy boothQrStrategy,
       final UUID userId) {
@@ -31,20 +33,27 @@ public class BoothService {
 
     UUID boothId = boothQrManager.getBoothIdFromQrId(qrId);
 
-    User user = getExistingUser(userId);
-    Booth booth = getExistingBooth(boothId);
+    User user = boothServiceUtils.getExistingUser(userId);
+    Booth booth = boothServiceUtils.getExistingBooth(boothId);
 
     user.visitBooth(booth);
     userJpaRepository.save(user);
   }
 
-  private User getExistingUser(final UUID userId) {
-    return userJpaRepository.findById(userId)
-        .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_ERROR));
-  }
+  public void updateBooth(final UUID boothId, final UpdateBoothRequest updateBoothRequest,
+      final UUID boothAdminId) {
+    Booth booth = boothServiceUtils.getExistingBooth(boothId);
+    Admin admin = boothServiceUtils.getExistingAdmin(boothAdminId);
 
-  private Booth getExistingBooth(final UUID boothId) {
-    return boothJpaRepository.findById(boothId)
-        .orElseThrow(() -> new NotFoundException(BOOTH_NOT_FOUND_ERROR));
+    boothServiceUtils.validateBoothAdminOwner(admin, booth);
+
+    doIfNotNull(updateBoothRequest.getName(), booth::updateName);
+    doIfNotNull(updateBoothRequest.getDescription(), booth::updateDescription);
+    doIfNotNull(updateBoothRequest.getLocation(), booth::updateLocation);
+    doIfNotNull(updateBoothRequest.getLocationImageUrl(),
+        url -> booth.updateLocationImage(new Image(url)));
+    doIfNotNull(updateBoothRequest.getImageUrl(), url -> booth.updateImage(new Image(url)));
+
+    boothJpaRepository.save(booth);
   }
 }
