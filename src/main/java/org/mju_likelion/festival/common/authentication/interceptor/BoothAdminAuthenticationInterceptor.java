d5b749c1d0af659c6320d23d303e9c9d7407c311
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 11:52:07 +0900

    feat: 부스 정보 조회 가능하도록 설정 - #84
    - /booths/{id} 는 부스 수정 URI 와 겹치기 때문

--- src/main/java/org/mju_likelion/festival/common/authentication/interceptor/BoothAdminAuthenticationInterceptor.java
+++ src/main/java/org/mju_likelion/festival/common/authentication/interceptor/BoothAdminAuthenticationInterceptor.java
@@ -2,12 +2,16 @@ package org.mju_likelion.festival.common.authentication.interceptor;
 
 import static org.mju_likelion.festival.common.exception.type.ErrorType.BOOTH_MANAGER_ONLY_ERROR;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 import org.mju_likelion.festival.auth.util.jwt.AuthenticationRole;
 import org.mju_likelion.festival.auth.util.jwt.JwtUtil;
 import org.mju_likelion.festival.auth.util.jwt.Payload;
 import org.mju_likelion.festival.common.authentication.AuthenticationContext;
+import org.mju_likelion.festival.common.config.RequestMatcher;
 import org.mju_likelion.festival.common.exception.type.ErrorType;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +20,14 @@ public class BoothAdminAuthenticationInterceptor extends AbstractAuthenticationI
   public BoothAdminAuthenticationInterceptor(AuthenticationContext authenticationContext,
       JwtUtil userJwtUtil) {
     super(authenticationContext, userJwtUtil);
+  }
+
+  @Override
+  protected List<RequestMatcher> getAllowedRequestMatchers() {
+    List<RequestMatcher> allowedRequestMatchers = new LinkedList<>();
+    allowedRequestMatchers.add(new RequestMatcher(HttpMethod.GET, "/booths/{id}"));
+
+    return allowedRequestMatchers;
   }
 
   @Override
