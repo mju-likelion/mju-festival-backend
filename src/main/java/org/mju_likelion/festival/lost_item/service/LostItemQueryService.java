acfdd843fdb8fe6dbd3758dd699e9772f37b119b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 14:23:06 +0900

    feat: 분실물 전체 조회 기능 구현 - #88

---
+++ src/main/java/org/mju_likelion/festival/lost_item/service/LostItemQueryService.java
@@ -0,0 +1,40 @@
+package org.mju_likelion.festival.lost_item.service;
+
+import static org.mju_likelion.festival.common.exception.type.ErrorType.PAGE_OUT_OF_BOUND_ERROR;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.mju_likelion.festival.common.exception.NotFoundException;
+import org.mju_likelion.festival.lost_item.domain.SimpleLostItem;
+import org.mju_likelion.festival.lost_item.domain.repository.LostItemQueryRepository;
+import org.mju_likelion.festival.lost_item.dto.response.SimpleLostItemsResponse;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LostItemQueryService {
+
+  private final LostItemQueryRepository lostItemQueryRepository;
+
+
+  public SimpleLostItemsResponse getLostItems(final Direction sort, final int page,
+      final int size) {
+
+    int totalPage = lostItemQueryRepository.findTotalPage(size);
+
+    validatePage(page, totalPage);
+
+    List<SimpleLostItem> simpleLostItems = lostItemQueryRepository.findOrderedSimpleLostItemsWithPagenation(
+        sort, page,
+        size);
+
+    return SimpleLostItemsResponse.of(simpleLostItems, totalPage);
+  }
+
+  private void validatePage(int page, int totalPage) {
+    if (page >= totalPage) {
+      throw new NotFoundException(PAGE_OUT_OF_BOUND_ERROR);
+    }
+  }
+}
