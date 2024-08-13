733925eeb52d4f590a4643bd5a57e10dd5980f5f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 11:44:46 +0900

    feat: NullHandler 구현 - #84
    - null 이 아닐 때만 함수를 실행하는 함수 구현

---
+++ src/main/java/org/mju_likelion/festival/common/util/null_handler/NullHandler.java
@@ -0,0 +1,21 @@
+package org.mju_likelion.festival.common.util.null_handler;
+
+import java.util.function.Consumer;
+
+/**
+ * Null 처리 유틸리티
+ */
+public class NullHandler {
+
+  /**
+   * 값이 null 이 아닐 때만 void 함수를 실행한다.
+   *
+   * @param value          값
+   * @param updateFunction 실행할 함수
+   */
+  public static <T> void doIfNotNull(T value, Consumer<T> updateFunction) {
+    if (value != null) {
+      updateFunction.accept(value);
+    }
+  }
+}
