ddd4b5e4853d27605607c40e8cddda15b7b7c00f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-13 17:02:34 +0900

    feat: 생성자, validation 메서드 추가 - #96

--- src/main/java/org/mju_likelion/festival/lost_item/domain/LostItem.java
+++ src/main/java/org/mju_likelion/festival/lost_item/domain/LostItem.java
@@ -1,5 +1,7 @@
 package org.mju_likelion.festival.lost_item.domain;
 
+import static org.mju_likelion.festival.common.exception.type.ErrorType.INVALID_LOST_ITEM_TITLE_LENGTH_ERROR;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,18 +10,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Transient;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.mju_likelion.festival.admin.domain.Admin;
 import org.mju_likelion.festival.common.domain.BaseEntity;
+import org.mju_likelion.festival.common.exception.BadRequestException;
+import org.mju_likelion.festival.common.exception.type.ErrorType;
+import org.mju_likelion.festival.common.util.string.StringUtil;
 import org.mju_likelion.festival.image.domain.Image;
 
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity(name = "lost_item")
 public class LostItem extends BaseEntity {
 
@@ -46,4 +47,53 @@ public class LostItem extends BaseEntity {
   @OneToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   @JoinColumn(nullable = false, name = "image_id")
   private Image image;
+
+  public LostItem(
+      final String title,
+      final String content,
+      final Image image,
+      final Admin writer) {
+
+    validate(title, content, image, writer);
+
+    this.title = title;
+    this.content = content;
+    this.image = image;
+    this.writer = writer;
+  }
+
+  private void validate(
+      final String title,
+      final String content,
+      final Image image,
+      final Admin writer) {
+    validateTitle(title);
+    validateContent(content);
+    validateImage(image);
+    validateWriter(writer);
+  }
+
+  private void validateTitle(final String title) {
+    if (StringUtil.isBlankOrLargerThan(title, TITLE_LENGTH)) {
+      throw new BadRequestException(INVALID_LOST_ITEM_TITLE_LENGTH_ERROR);
+    }
+  }
+
+  private void validateContent(final String content) {
+    if (StringUtil.isBlankOrLargerThan(content, CONTENT_LENGTH)) {
+      throw new BadRequestException(ErrorType.INVALID_LOST_ITEM_CONTENT_LENGTH_ERROR);
+    }
+  }
+
+  private void validateImage(final Image image) {
+    if (image == null) {
+      throw new BadRequestException(ErrorType.LOST_ITEM_IMAGE_MISSING_ERROR);
+    }
+  }
+
+  private void validateWriter(final Admin writer) {
+    if (writer == null) {
+      throw new BadRequestException(ErrorType.LOST_ITEM_WRITER_MISSING_ERROR);
+    }
+  }
 }
