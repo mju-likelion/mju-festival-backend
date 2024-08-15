694af618409cfc7b6423eba8730baa46dae7b953
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-16 02:34:08 +0900

    feat: Health Check 용 Controller 추가 - #110

---
+++ src/main/java/org/mju_likelion/festival/common/health_check/HealthController.java
@@ -0,0 +1,14 @@
+package org.mju_likelion.festival.common.health_check;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/health")
+  public ResponseEntity<String> health() {
+    return ResponseEntity.ok().build();
+  }
+}
