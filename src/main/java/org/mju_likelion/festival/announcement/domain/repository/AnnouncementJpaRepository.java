80bc85ad4610d2861cee1c5405d940a1f55de644
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-19 23:37:25 +0900

    refactor: 테스트 용도 메서드 제거 - #120
    - 테스트에서만 사용할 Repository 메서드는 Test 패키지에서 선언하도록 변경 예정

--- src/main/java/org/mju_likelion/festival/announcement/domain/repository/AnnouncementJpaRepository.java
+++ src/main/java/org/mju_likelion/festival/announcement/domain/repository/AnnouncementJpaRepository.java
@@ -1,6 +1,5 @@
 package org.mju_likelion.festival.announcement.domain.repository;
 
-import java.util.List;
 import java.util.UUID;
 import org.mju_likelion.festival.announcement.domain.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,10 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AnnouncementJpaRepository extends JpaRepository<Announcement, UUID> {
 
-  /**
-   * 작성자를 기준으로 공지사항을 조회한다.
-   * <p>
-   * 테스트에서만 사용한다.
-   */
-  List<Announcement> findByWriterId(final UUID writerId);
 }
--- src/main/java/org/mju_likelion/festival/lost_item/domain/repository/LostItemJpaRepository.java
+++ src/main/java/org/mju_likelion/festival/lost_item/domain/repository/LostItemJpaRepository.java
@@ -1,6 +1,5 @@
 package org.mju_likelion.festival.lost_item.domain.repository;
 
-import java.util.List;
 import java.util.UUID;
 import org.mju_likelion.festival.lost_item.domain.LostItem;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LostItemJpaRepository extends JpaRepository<LostItem, UUID> {
 
-  /**
-   * 작성자 ID로 분실물을 조회한다.
-   * <p>
-   * 테스트에서만 사용
-   *
-   * @param writerId 작성자 ID
-   * @return 분실물 목록
-   */
-  List<LostItem> findByWriterId(final UUID writerId);
 }
