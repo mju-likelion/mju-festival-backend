7a704df3c4de6f82a9adca147be14b78dcc22854
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-19 23:35:17 +0900

    refactor: 기본 생성자를 이용하도록 변경 - #120

--- src/main/java/org/mju_likelion/festival/term/domain/TermUsers.java
+++ src/main/java/org/mju_likelion/festival/term/domain/TermUsers.java
@@ -6,19 +6,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.ToString;
 import org.mju_likelion.festival.user.domain.User;
 
 @Embeddable
+@ToString(callSuper = true, of = {"termUsers"})
 public class TermUsers {
 
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private Set<TermUser> termUsers = new HashSet<>();
 
   public void agree(final Term term, final User user) {
-    TermUser termUser = TermUser.builder()
-        .term(term)
-        .user(user)
-        .build();
+    TermUser termUser = new TermUser(user, term);
     this.termUsers.add(termUser);
   }
 }
