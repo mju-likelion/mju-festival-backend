8ead75f6e4ca153d7c877526f2d3c817a2fbcc42
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-27 23:56:35 +0900

    refactor: 날짜 제한에 포함시킬  URI 등록 - #137
    - 유저 로그인
    - 부스 qr 발급
    - 부스 qr 참여

--- src/main/java/org/mju_likelion/festival/common/filter/date_restriction/DateRestrictionFilter.java
+++ src/main/java/org/mju_likelion/festival/common/filter/date_restriction/DateRestrictionFilter.java
@@ -1,5 +1,8 @@
 package org.mju_likelion.festival.common.filter.date_restriction;
 
+import static org.mju_likelion.festival.common.api.ApiPaths.ISSUE_BOOTH_QR;
+import static org.mju_likelion.festival.common.api.ApiPaths.USER_LOGIN;
+import static org.mju_likelion.festival.common.api.ApiPaths.VISIT_BOOTH;
 import static org.mju_likelion.festival.common.exception.type.ErrorType.DATE_RESTRICTED_ERROR;
 
 import jakarta.servlet.Filter;
@@ -8,9 +11,14 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
 import org.mju_likelion.festival.common.exception.ServiceUnavailableException;
+import org.mju_likelion.festival.common.util.request_matcher.RequestMatcher;
+import org.springframework.http.HttpMethod;
 
 public class DateRestrictionFilter implements Filter {
 
@@ -32,11 +40,37 @@ public class DateRestrictionFilter implements Filter {
       final ServletResponse response,
       final FilterChain chain) throws IOException, ServletException {
 
+    if (isPassableRequest(request)) {
+      chain.doFilter(request, response);
+      return;
+    }
     LocalDate currentDate = LocalDate.now();
 
     if (currentDate.isBefore(startDate) || currentDate.isAfter(endDate)) {
       throw new ServiceUnavailableException(DATE_RESTRICTED_ERROR);
     }
     chain.doFilter(request, response);
+  }
+
+  private boolean isPassableRequest(final ServletRequest request) {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    return isOptionsRequest(httpRequest) || !isRequestNotPermitted(httpRequest);
+  }
+
+  private boolean isOptionsRequest(final HttpServletRequest request) {
+    return Objects.equals(request.getMethod(), "OPTIONS");
+  }
+
+  private boolean isRequestNotPermitted(final HttpServletRequest request) {
+    return getNotAllowedRequestMatchers().stream()
+        .anyMatch(matcher -> matcher.matches(request));
+  }
+
+  private List<RequestMatcher> getNotAllowedRequestMatchers() {
+    return List.of(
+        new RequestMatcher(HttpMethod.POST, USER_LOGIN),
+        new RequestMatcher(HttpMethod.GET, ISSUE_BOOTH_QR),
+        new RequestMatcher(HttpMethod.POST, VISIT_BOOTH)
+    );
   }
 }
