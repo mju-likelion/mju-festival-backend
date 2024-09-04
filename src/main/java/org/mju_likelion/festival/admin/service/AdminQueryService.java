4d56dd394b83aff9e82199f4b4cdf2202706ca33
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-04 13:50:43 +0900

    feat: 역할 분리를 위한 Admin Query 서비스 구현 - #142

--- src/main/java/org/mju_likelion/festival/admin/service/AdminQueryService.java
+++ src/main/java/org/mju_likelion/festival/admin/service/AdminQueryService.java
@@ -1,32 +1,23 @@
-package org.mju_likelion.festival.announcement.service;
+package org.mju_likelion.festival.admin.service;
 
 import static org.mju_likelion.festival.common.exception.type.ErrorType.ADMIN_NOT_FOUND_ERROR;
-import static org.mju_likelion.festival.common.exception.type.ErrorType.ANNOUNCEMENT_NOT_FOUND_ERROR;
-import static org.mju_likelion.festival.common.exception.type.ErrorType.PAGE_OUT_OF_BOUND_ERROR;
 
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.mju_likelion.festival.admin.domain.Admin;
 import org.mju_likelion.festival.admin.domain.repository.AdminJpaRepository;
-import org.mju_likelion.festival.announcement.domain.Announcement;
-import org.mju_likelion.festival.announcement.domain.repository.AnnouncementJpaRepository;
 import org.mju_likelion.festival.common.exception.NotFoundException;
+import org.mju_likelion.festival.common.exception.UnauthorizedException;
+import org.mju_likelion.festival.common.exception.type.ErrorType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class AnnouncementServiceUtil {
+public class AdminQueryService {
 
-  private final AnnouncementJpaRepository announcementJpaRepository;
   private final AdminJpaRepository adminJpaRepository;
-
-  public void validatePage(final int page, final int totalPage) {
-    if (page != 0 && page >= totalPage) {
-      throw new NotFoundException(PAGE_OUT_OF_BOUND_ERROR);
-    }
-  }
 
   public void validateAdminExistence(final UUID adminId) {
     if (!adminJpaRepository.existsById(adminId)) {
@@ -39,8 +30,8 @@ public class AnnouncementServiceUtil {
         .orElseThrow(() -> new NotFoundException(ADMIN_NOT_FOUND_ERROR));
   }
 
-  public Announcement getExistingAnnouncement(final UUID announcementId) {
-    return announcementJpaRepository.findById(announcementId)
-        .orElseThrow(() -> new NotFoundException(ANNOUNCEMENT_NOT_FOUND_ERROR));
+  public Admin getExistingAdmin(final String loginId, final String password) {
+    return adminJpaRepository.findByLoginIdAndPassword(loginId, password)
+        .orElseThrow(() -> new UnauthorizedException(ErrorType.INVALID_CREDENTIALS_ERROR));
   }
 }
