df7853d3a9d6e5b36d2cffb9bbf0ad15bd48da3b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-04 13:51:12 +0900

    feat: 역할 분리를 위한 User 서비스 구현 - #142

---
+++ src/main/java/org/mju_likelion/festival/user/service/UserService.java
@@ -0,0 +1,51 @@
+package org.mju_likelion.festival.user.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.mju_likelion.festival.term.domain.Term;
+import org.mju_likelion.festival.user.domain.User;
+import org.mju_likelion.festival.user.domain.repository.UserJpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+  private final UserJpaRepository userJpaRepository;
+
+  /**
+   * 사용자가 동의한 약관을 저장하고, 사용자의 ID를 반환한다.
+   * <p>
+   * 사용자가 이미 존재하는 경우, 사용자의 ID를 반환한다.
+   *
+   * @param studentId 학번
+   * @param terms     사용자가 동의한 약관
+   * @return 사용자의 ID
+   */
+  public UUID saveOrGetUserId(final String studentId, final List<Term> terms) {
+    Optional<UUID> userId = userJpaRepository.findIdByStudentId(studentId);
+
+    return userId.orElseGet(() -> saveUser(studentId, terms));
+  }
+
+  /**
+   * 사용자를 저장하고, 사용자의 ID를 반환한다.
+   *
+   * @param studentId 학번
+   * @param terms     사용자가 동의한 약관
+   * @return 사용자의 ID
+   */
+  private UUID saveUser(final String studentId, final List<Term> terms) {
+    User user = new User(studentId);
+
+    terms.forEach(user::agreeToTerm);
+
+    userJpaRepository.save(user);
+
+    return user.getId();
+  }
+}
