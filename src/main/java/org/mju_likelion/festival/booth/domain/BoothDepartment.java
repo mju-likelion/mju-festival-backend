b3dd2995467f239a5434f975bc08002cf14b688f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-13 23:10:28 +0900

    feat: 부스 소속 Entity 구현 - #155

---
+++ src/main/java/org/mju_likelion/festival/booth/domain/BoothDepartment.java
@@ -0,0 +1,47 @@
+package org.mju_likelion.festival.booth.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.mju_likelion.festival.common.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true, of = {"name", "categoryName"})
+@Entity(name = "booth_department")
+public class BoothDepartment extends BaseEntity {
+
+  @Transient
+  private final int NAME_LENGTH = 15;
+  @Transient
+  private final int CATEGORY_NAME_LENGTH = 15;
+
+  @Column(nullable = false, length = NAME_LENGTH, unique = true)
+  private String name;
+
+  @Column(nullable = false, length = CATEGORY_NAME_LENGTH, unique = true)
+  private String categoryName;
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    final BoothDepartment that = (BoothDepartment) o;
+    return Objects.equals(name, that.name) && Objects.equals(categoryName, that.categoryName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name) + Objects.hash(categoryName);
+  }
+}
