6f5b5ba1a7c502151de484ca00b85c49c6bcbf6a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-19 18:43:38 +0900

    refactor: 테스트를 위한 AllArgsConstructor 추가 - #171

--- src/main/java/org/mju_likelion/festival/lost_item/dto/request/LostItemFoundRequest.java
+++ src/main/java/org/mju_likelion/festival/lost_item/dto/request/LostItemFoundRequest.java
@@ -1,6 +1,7 @@
 package org.mju_likelion.festival.lost_item.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
  */
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LostItemFoundRequest {
 
   @NotBlank(message = "수령인 정보는 필수값입니다.")
