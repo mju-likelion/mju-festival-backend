1e3bbe059ab57d3685a5f8ff508de7838483eda6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-19 18:40:21 +0900

    fix: 기본 생성자를 사용하도록 변경 - #171
    - 기본 생성자가 없어 직렬화 시 문제 발생

--- src/main/java/org/mju_likelion/festival/lost_item/dto/request/LostItemFoundRequest.java
+++ src/main/java/org/mju_likelion/festival/lost_item/dto/request/LostItemFoundRequest.java
@@ -1,14 +1,14 @@
 package org.mju_likelion.festival.lost_item.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 분실물 찾음 처리 요청 DTO
  */
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class LostItemFoundRequest {
 
   @NotBlank(message = "수령인 정보는 필수값입니다.")
