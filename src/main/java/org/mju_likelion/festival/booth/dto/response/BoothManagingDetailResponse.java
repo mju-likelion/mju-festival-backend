274082ebc955dce598b71d3b13af9cdf763a6390
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-25 22:46:58 +0900

    refactor: toString 에 isEventBooth 추가 - #180

--- src/main/java/org/mju_likelion/festival/booth/dto/response/BoothManagingDetailResponse.java
+++ src/main/java/org/mju_likelion/festival/booth/dto/response/BoothManagingDetailResponse.java
@@ -22,6 +22,7 @@ public class BoothManagingDetailResponse {
   public String toString() {
     return "BoothOwnershipResponse{" +
         "isOwner=" + isOwner +
+        ", isEventBooth=" + isEventBooth +
         '}';
   }
 }
