4a95e2772f1c678cbd1977744723819afad7c758
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-26 19:47:13 +0900

    refactor: RestFilter 를 상속받도록 변경 - #190

--- src/main/java/org/mju_likelion/festival/common/filter/date_restriction/DateRestrictionFilter.java
+++ src/main/java/org/mju_likelion/festival/common/filter/date_restriction/DateRestrictionFilter.java
@@ -5,22 +5,16 @@ import static org.mju_likelion.festival.common.api.ApiPaths.USER_LOGIN;
 import static org.mju_likelion.festival.common.api.ApiPaths.VISIT_BOOTH;
 import static org.mju_likelion.festival.common.exception.type.ErrorType.DATE_RESTRICTED_ERROR;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import org.mju_likelion.festival.common.exception.ServiceUnavailableException;
+import org.mju_likelion.festival.common.filter.RestFilter;
 import org.mju_likelion.festival.common.util.request_matcher.RequestMatcher;
 import org.springframework.http.HttpMethod;
 
-public class DateRestrictionFilter implements Filter {
+public class DateRestrictionFilter extends RestFilter {
 
   private LocalDate startDate;
   private LocalDate endDate;
@@ -35,38 +29,16 @@ public class DateRestrictionFilter implements Filter {
   }
 
   @Override
-  public void doFilter(
-      final ServletRequest request,
-      final ServletResponse response,
-      final FilterChain chain) throws IOException, ServletException {
-
-    if (isPassableRequest(request)) {
-      chain.doFilter(request, response);
-      return;
-    }
+  protected void process(final ServletRequest request) {
     LocalDate currentDate = LocalDate.now();
 
     if (currentDate.isBefore(startDate) || currentDate.isAfter(endDate)) {
       throw new ServiceUnavailableException(DATE_RESTRICTED_ERROR);
     }
-    chain.doFilter(request, response);
   }
 
-  private boolean isPassableRequest(final ServletRequest request) {
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    return isOptionsRequest(httpRequest) || !isRequestNotPermitted(httpRequest);
-  }
-
-  private boolean isOptionsRequest(final HttpServletRequest request) {
-    return Objects.equals(request.getMethod(), "OPTIONS");
-  }
-
-  private boolean isRequestNotPermitted(final HttpServletRequest request) {
-    return getNotAllowedRequestMatchers().stream()
-        .anyMatch(matcher -> matcher.matches(request));
-  }
-
-  private List<RequestMatcher> getNotAllowedRequestMatchers() {
+  @Override
+  protected List<RequestMatcher> getNotAllowedRequestMatchers() {
     return List.of(
         new RequestMatcher(HttpMethod.POST, USER_LOGIN),
         new RequestMatcher(HttpMethod.GET, ISSUE_BOOTH_QR),
