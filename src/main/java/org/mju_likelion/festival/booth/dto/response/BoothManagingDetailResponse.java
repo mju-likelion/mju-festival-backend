1264b2cab211d476da337ad1e99357294f442725
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-02 16:18:19 +0900

    refactor: 역직렬화를 위한 NoArgsConstructor 추가 - #198

--- src/main/java/org/mju_likelion/festival/booth/dto/response/BoothManagingDetailResponse.java
+++ src/main/java/org/mju_likelion/festival/booth/dto/response/BoothManagingDetailResponse.java
@@ -3,16 +3,18 @@ package org.mju_likelion.festival.booth.dto.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 부스 소유권 응답 DTO.
  */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class BoothManagingDetailResponse {
 
-  private final Boolean isOwner;
-  private final Boolean isEventBooth;
+  private Boolean isOwner;
+  private Boolean isEventBooth;
 
   public static BoothManagingDetailResponse of(final boolean isOwner, final boolean isEventBooth) {
     return new BoothManagingDetailResponse(isOwner, isEventBooth);
